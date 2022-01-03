f187f0a31c99545521c11d5f396cbdff500ba7fd
luiscarriel <vitorluiiscarriel@gmail.com>
2022-01-03 13:54:59 -0500

    update

---
+++ ex13/script.js
@@ -0,0 +1,19 @@
+// Laço for..in
+
+const pessoa = {
+    nome: "Luis",
+    idade: 17
+};
+
+// key-value
+for(let chave in pessoa){
+    console.log(chave, pessoa["nome"]);
+}
+
+// cores 
+
+const cores = ["vermelho", "Azul", "Verde"];
+
+for(let indice in cores){
+    console.log(indice, cores[indice])
+}
